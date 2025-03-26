228c2905c5f1c75941b7ce195c5e1c0e8e9d163d
DineJ <dinejridi@gmail.com>
2025-03-26 08:36:36 +0100

    Changing pathing local files

---
+++ create_crud.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir -p app/Entities
+
+php spark generate:models $1
+php spark generate:entity "${1}Model"
+php spark generate:controller $1
+php spark generate:views $1
+php spark generate:views $1
+php spark generate:routes $1
