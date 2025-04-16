c3b29b5d00edbbdb15fc4b77e1c5a978d22d82d7
DineJ <dinejridi@gmail.com>
2025-04-16 11:56:50 +0200

    improve script. Can now delete files for testing.

--- create_crud.sh
+++ create_crud.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
-mkdir -p app/Entities
 
-php spark generate:models $1
-php spark generate:entity "${1}Model"
-php spark generate:controller $1
-php spark generate:views $1
-php spark generate:routes $1
+function help()
+{
+	scriptname=$(basename "$0")
+	echo "How to use ${scriptname} : ${scriptname} create <entityname> [field1,field2,field3,...]"
+	echo "                            ${scriptname} rm <entityname>"
+}
+
+# arg=un deux trois quatre
+action=$1
+shift
+# arg=deux trois quatre
+case $action in
+
+ "create")
+	if [ $# -lt 1 ]; then
+		echo "Nombre d'argument insuffisant"
+	 	help
+	else
+		mkdir -p app/Entities
+
+		php spark generate:models $@
+		php spark generate:entity "${1}Model"
+		php spark generate:controller $@
+		php spark generate:views $@
+		php spark generate:routes $@
+	fi
+    ;;
+
+  "rm")
+	if [ $# -ne 1 ]; then
+		echo "Nombre d'argument insuffisant"
+		help
+	else
+		entity=${1^}
+		rm -riv app/Views/$entity/*.php  app/Entities/${entity}.php app/Models/${entity}Model.php app/Controllers/${entity}Controller.php
+	fi
+    ;;
+esac
+
