356ca75b030f88b7219392af4a81d9d1247299aa
DineJ <dinejridi@gmail.com>
2025-05-20 14:51:47 +0200

    Added patterns, corrected data format (data time). Added JS script to pre-fill 'select' and is now required too

--- app/Views/Permis/edit.php
+++ app/Views/Permis/edit.php
@@ -5,40 +5,51 @@
 
 <form method="post" action="<?= site_url('Permis/update/'.$item->id_user) ?>" onsubmit="return validateForm()">
 
+	<input type="hidden" name="id_user" id="id_user" value="<?= isset($item) ? $item->id_user : -1 ?>">
+
 	<label>num_permis</label>
-	<input type='text' onchange="setUpper(document.getElementById('num_permis'));" id='num_permis' name='num_permis' value='<?= isset($item) ? $item->num_permis : '' ?>' class='form-control' required>
+	<input type='text' pattern="^[0-9]{10}" id='num_permis' name='num_permis' value='<?= isset($item) ? $item->num_permis : '' ?>' class='form-control' required>
 	<input type='hidden' id='oldnum_permis' name='oldnum_permis' value='<?= isset($item) ? $item->num_permis : '' ?>'>
 
 	<label>date_permis</label>
-	<input type='date' id='date_permis' name='date_permis' value='<?= isset($item) ? $item->date_permis : '' ?>' class='form-control' required>
-	<input type='hidden' id='olddate_permis' name='olddate_permis' value='<?= isset($item) ? $item->date_permis : '' ?>'>
+	<input type='date' id='date_permis' name='date_permis' value='<?= isset($item) ? substr($item->date_permis, 0, 10) : '' ?>' class='form-control' required>
+	<input type='hidden' id='olddate_permis' name='olddate_permis' value='<?= isset($item) ? substr($item->date_permis, 0, 10) : '' ?>'>
 
 	<label>update_permis</label>
-	<input type='date' id='update_permis' name='update_permis' value='<?= isset($item) ? $item->update_permis : '' ?>' class='form-control' required>
-	<input type='hidden' id='oldupdate_permis' name='oldupdate_permis' value='<?= isset($item) ? $item->update_permis : '' ?>'>
+	<input type='date' id='update_permis' name='update_permis' value='<?= isset($item) ? substr($item->update_permis, 0, 10) : '' ?>' class='form-control' required>
+	<input type='hidden' id='oldupdate_permis' name='oldupdate_permis' value='<?= isset($item) ? substr($item->update_permis, 0, 10) : '' ?>'>
 
 	<label>type_permis</label>
 	<div>
-		<select id='type_permis' name='type_permis'>
-			<option>--Please choose an option--</option>
-			<option value=B>B</option>
-			<option value=BE>BE</option>
-			<option value=C>C</option>
-			<option value=C1>C1</option>
-			<option value=C1E>C1E</option>
+		<select id="type_permis" name="type_permis" required>
+			<option value="" disabled selected hidden>--Veuillez choisir un type de permis--</option>
+			<option value="B">B</option>
+			<option value="BE">BE</option>
+			<option value="C">C</option>
+			<option value="C1">C1</option>
+			<option value="C1E">C1E</option>
 		</select>
 	</div>
 	<input type='hidden' id='oldtype_permis' name='oldtype_permis' value='<?= isset($item) ? $item->type_permis : '' ?>'>
+
 
 	<a href="<?= site_url('Permis') ?>" class="btn btn-secondary mt-3">Retour</a>
 	<button type="submit" class="btn btn-primary mt-3">Enregistrer</button>
 </form>
 
 <script>
-	function setUpper(element)
+	window.addEventListener('DOMContentLoaded', function ()
 	{
-		element.value=element.value.toUpperCase();
+		const oldValue = document.getElementById('oldtype_permis').value;
+		const select = document.getElementById('type_permis');
+
+		if (oldValue)
+		{
+		  select.value = oldValue;
+		}
 	}
+	);
+
 	function validateForm()
 	{
 		let compare = 0;
