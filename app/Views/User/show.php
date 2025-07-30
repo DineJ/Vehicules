a4c512bc43adbec6462a6ef8ae63e7ef16f6282a
DineJ <dinejridi@gmail.com>
2025-07-30 10:07:12 +0200

    If a user doesn't have a license (permis), they can create one from the view. Customize the style for better usability.

--- app/Views/User/show.php
+++ app/Views/User/show.php
@@ -2,7 +2,7 @@
 <?= $this->section('content') ?>
 
 <div class="container mt-5">
-<h2>Détails de User</h2>
+<h2>Détails de l'utilisateur</h2>
 
 <table class="table table-striped table-bordered">
 	<tbody>
@@ -33,19 +33,40 @@
 	</tbody>
 </table>
 
-<?php if (!isset($permis)) { ?>
-<a href="<?= site_url('Permis/create/'.$item->id) ?>" class="btn btn-secondary">Permis</a>
+<div>
 
-<?php 
-} 
-else 
-{
-?>
+	<form method="post" action="<?= site_url('User/update/'.$item->id) ?>">
+
+		<a href="<?= site_url('User/edit/'.$item->id) ?>" class="btn btn-warning">Modifier</a>
+		<input type="hidden" name="actif" id="actif" value="<?= $item->actif ? 0 : 1 ?>">
+		<button type="submit" class="btn <?= $item->actif ? 'btn-danger' : 'btn-success' ?>"> <?= $item->actif ? 'Rendre inactif' : 'Rendre actif' ?></button>
+	</form>
+</div>
+
 <div style="margin-left: 3rem; margin-top: 1.5rem; width: 85%; padding: 1rem; border: 1px solid #ccc; border-left: 4px solid #6f42c1; border-radius: 8px;">
-	<h6 style="margin-bottom: 1rem; color: #6f42c1;">↳ Permis</h6>
+
+	<div style="display: flex; align-items: center; justify-content: space-between; margin-bottom: 1rem;">
+		<h6 style="margin: 0; color: #6f42c1;">↳ Permis</h6>
+
+		<?php if (!isset($permis)) { ?>
+		<a href="<?= site_url('Permis/create/'.$item->id) ?>" class="btn btn-success">Ajouter</a>
+	</div>
+		<table class="table table-striped table-bordered">
+			<tbody>
+				<tr>
+					<td>Pas de permis enregistrer</td>
+				</tr>
+	<?php
+	}
+	else
+	{
+	?>
+	<a href="<?= site_url('Permis/edit/' . $permis->id_user) ?>" class="btn btn-warning">Modifer</a>
+</div>
+
 	<table class="table table-striped table-bordered">
 		<tbody>
-				<tr>
+			<tr>
 				<td>Numéro</td>
 				<td><?= $permis->num_permis ?></td>
 			</tr>
@@ -61,28 +82,15 @@ else
 				<td>Catégorie</td>
 				<td><?= $permis->type_permis ?></td>
 			</tr>
+
+	<?php
+	}
+	?>
 		</tbody>
 	</table>
-
-	<div>
-		<form method="post" action="<?= site_url('Permis/update/'.$permis->id_user) ?>">
-			<a href="<?= site_url('Permis/edit/'.$permis->id_user) ?>" class="btn btn-warning">Modifier permis</a>
-		</form>
-	</div>
 </div>
-<?php
-}
-?>
 
 </br>
 
-<div>
-	<form method="post" action="<?= site_url('User/update/'.$item->id) ?>">
-		<a href="<?= previous_url() ?>" class="btn btn-secondary">Retour</a>
-		<a href="<?= site_url('User/edit/'.$item->id) ?>" class="btn btn-warning">Modifier utilisateur</a>
-		<input type="hidden" name="actif" id="actif" value="<?= $item->actif ? 0 : 1 ?>">
-		<button type="submit" class="btn <?= $item->actif ? 'btn-danger' : 'btn-success' ?>"> <?= $item->actif ? 'Rendre inactif' : 'Rendre actif' ?></button>
-	</form>
-</div>
-
+<a href="<?= previous_url() ?>" class="btn btn-secondary">Retour</a>
 <?= $this->endSection() ?>
