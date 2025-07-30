b05af81963ae5ad486cb5b9d1d371cb5f35005f9
DineJ <dinejridi@gmail.com>
2025-07-30 11:09:21 +0200

    Corrected labels and return button

--- app/Views/Permis/edit.php
+++ app/Views/Permis/edit.php
@@ -1,28 +1,27 @@
 <?= $this->extend('layouts/main') ?>
 <?= $this->section('content') ?>
 
-<h2>Permis - <?= $title ?></h2>
+<h2><?= $title ?></h2>
 
 <form method="post" action="<?= site_url('Permis/update/'.$item->id_user) ?>" onsubmit="return validateForm()">
 
 	<input type="hidden" name="id_user" id="id_user" value="<?= isset($item) ? $item->id_user : -1 ?>">
 
-	<label>num_permis</label>
+	<label>Numéro</label>
 	<input type='text' pattern="^[0-9]{10}" id='num_permis' name='num_permis' value='<?= isset($item) ? $item->num_permis : '' ?>' class='form-control' required>
 	<input type='hidden' id='oldnum_permis' name='oldnum_permis' value='<?= isset($item) ? $item->num_permis : '' ?>'>
 
-	<label>date_permis</label>
+	<label>Date d'obtention</label>
 	<input type='date' id='date_permis' name='date_permis' value='<?= isset($item) ? substr($item->date_permis, 0, 10) : '' ?>' class='form-control' required>
 	<input type='hidden' id='olddate_permis' name='olddate_permis' value='<?= isset($item) ? substr($item->date_permis, 0, 10) : '' ?>'>
 
-	<label>update_permis</label>
+	<label>Date d'expiration</label>
 	<input type='date' id='update_permis' name='update_permis' value='<?= isset($item) ? substr($item->update_permis, 0, 10) : '' ?>' class='form-control' required>
 	<input type='hidden' id='oldupdate_permis' name='oldupdate_permis' value='<?= isset($item) ? substr($item->update_permis, 0, 10) : '' ?>'>
 
-	<label>type_permis</label>
+	<label>Catégorie</label>
 	<div>
 		<select id="type_permis" name="type_permis" required>
-			<option value="" disabled selected hidden>--Veuillez choisir un type de permis--</option>
 			<option value="B">B</option>
 			<option value="BE">BE</option>
 			<option value="C">C</option>
@@ -33,7 +32,7 @@
 	<input type='hidden' id='oldtype_permis' name='oldtype_permis' value='<?= isset($item) ? $item->type_permis : '' ?>'>
 
 
-	<a href="<?= site_url('Permis') ?>" class="btn btn-secondary mt-3">Retour</a>
+	<a href="<?= previous_url() ?>" class="btn btn-secondary mt-3">Retour</a>
 	<button type="submit" class="btn btn-primary mt-3">Enregistrer</button>
 </form>
 
