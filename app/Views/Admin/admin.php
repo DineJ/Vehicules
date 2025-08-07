9631b2fa7752b360197730da6dd32c293579f0a4
DineJ <dinejridi@gmail.com>
2025-08-07 11:47:51 +0200

    Fixed css style. Remove <table> from loops, fixed responsive style to align titles and labels correctly

--- app/Views/Admin/admin.php
+++ app/Views/Admin/admin.php
@@ -3,67 +3,68 @@
 
 <!-- Main content -->
 <div class="container mt-4">
-	<h2>Bienvenue sur l’espace admin</h2>
+	<h2 style="text-align: center;">Bienvenue sur l’espace admin</h2>
 </div>
 
+</br>
+
 <div class="table-responsive">
-	<h4> Adresse IP Bannies </h4>
+	<h4 class="centerTitle"> Adresse IP Bannies </h4>
+	<table class="table table-striped table-bordered mt-3">
+		<tbody>
+			<!-- Test if atleast one ip is banned -->
+			<?php if (empty($ip)) { ?>
 
-		<!-- Test if atleast one ip is banned -->
-		<?php if (empty($ip)) { ?>
-			<table class="table table-striped table-bordered mt-3">
-				<tbody>
-					<!-- None banned IP -->
-					<tr>
-						<td>Aucune adresse IP bannie</td>
-					</tr>
-		<?php
-		}
-		else
-		{
-		?>
-			<!-- Display banned ip -->
-			<?php foreach ($ip as $ips): ?>
-				<table class="table table-striped table-bordered mt-3">
-					<tbody>
-						<tr>
-							<td class="td-hidden table15pourcent">Adresse IP</td>
-							<td data-label="Adresse Ip"><?= esc($ips->adresse_ip) ?></td>
-							<td data-label="Action" class="actionend">
-
-								<form method="post" action="<?= site_url('Ip/update/'.$ips->id) ?>">
-									<!-- Enabled ip button -->
-									<input type="hidden" name="nb_echec" id="nb_echec" value="0"?>
-
-									<!-- Redirection button -->
-									<input type="hidden" name="redirect_url" value="<?= current_url(); ?>">
-									<button type="submit" class="btn btn-danger btn-sm"> Rétablir IP </button>
-								</form>
-							</td>
-						</tr>
-			<?php endforeach; ?>
+				<!-- None banned IP -->
+				<tr>
+					<td class="labelAlign">Aucune adresse IP bannie</td>
+				</tr>
 			<?php
 			}
+			else
+			{
 			?>
+				<!-- Display banned ip -->
+				<?php foreach ($ip as $ips): ?>
+					<tr>
+						<td class="td-hidden table15pourcent">Adresse IP</td>
+						<td data-label="Adresse Ip"><?= esc($ips->adresse_ip) ?></td>
+						<td data-label="Action" class="actionend">
 
+							<form method="post" action="<?= site_url('Ip/update/'.$ips->id) ?>">
+								<!-- Enabled ip button -->
+								<input type="hidden" name="nb_echec" id="nb_echec" value="0"?>
+
+								<!-- Redirection button -->
+								<input type="hidden" name="redirect_url" value="<?= current_url(); ?>">
+								<button type="submit" class="btn btn-danger btn-sm"> Rétablir IP </button>
+							</form>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+				<?php
+				}
+				?>
 		</tbody>
 	</table>
 
 	<!-- Redirection button -->
 	<a href="<?= site_url('Ip') ?>" class="btn btn-secondary">Retour vers IP</a>
 
-	<br><br>
+	</br></br>
 
-	<h4> Utilisateur Bannis </h4>
+	<h4 class="centerTitle"> Utilisateur Bannis </h4>
+	<table class="table table-striped table-bordered mt-3">
+		<tbody>
 
-	<!-- Test if atleast one user is banned -->
-	<?php if (empty($user)) { ?>
-		<table class="table table-striped table-bordered mt-3">
-				<tbody>
-					<!-- None banned user -->
-					<tr>
-						<td>Aucun utilisateur banni</td>
-					</tr>
+		<!-- Test if atleast one user is banned -->
+		<?php if (empty($user)) { ?>
+
+			<!-- None banned user -->
+			<tr>
+				<td class="labelAlign">Aucun utilisateur banni</td>
+			</tr>
+
 		<?php
 		}
 		else
@@ -71,28 +72,25 @@
 		?>
 			<!-- Display banned user -->
 			<?php foreach ($user as $users): ?>
-				<table class="table table-striped table-bordered mt-3 noBorder">
-					<tbody>
-						<tr>
-							<td data-label="Nom" class="table15pourcent"><?= esc($users->nom) ?></td>
-							<td data-label="Prénom"><?= esc($users->prenom) ?></td>
-							<td data-label="Action" class="actionend">
+				<tr>
+					<td data-label="Nom" class="table15pourcent"><?= esc($users->nom) ?></td>
+					<td data-label="Prénom"><?= esc($users->prenom) ?></td>
+					<td data-label="Action" class="actionend">
 
-								<form method="post" action="<?= site_url('User/update/'.$users->id) ?>">
-									<!-- Enabled account button -->
-									<input type="hidden" name="actif" id="actif" value="1">
+						<form method="post" action="<?= site_url('User/update/'.$users->id) ?>">
+							<!-- Enabled account button -->
+							<input type="hidden" name="actif" id="actif" value="1">
 
-									<!-- Redirection button -->
-									<input type="hidden" name="redirect_url" value="<?= current_url(); ?>">
-									<button type="submit" class="btn btn-danger btn-sm"> Rétablir utilisateur </button>
-								</form>
-							</td>
-						</tr>
+							<!-- Redirection button -->
+							<input type="hidden" name="redirect_url" value="<?= current_url(); ?>">
+							<button type="submit" class="btn btn-danger btn-sm"> Rétablir utilisateur </button>
+						</form>
+					</td>
+				</tr>
 			<?php endforeach; ?>
 			<?php
 			}
 			?>
-
 		<t/body>
 	</table>
 
