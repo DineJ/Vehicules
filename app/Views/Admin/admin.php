4063a2f529bac0d6a856182e0f6664c89e51a7eb
DineJ <dinejridi@gmail.com>
2025-08-07 11:02:29 +0200

    Display a label if neither  user nor IP are banned

--- app/Views/Admin/admin.php
+++ app/Views/Admin/admin.php
@@ -9,27 +9,43 @@
 <div class="table-responsive">
 	<h4> Adresse IP Bannies </h4>
 
-	<!-- Display banned ip -->
-	<table class="table table-striped table-bordered mt-3">
-		<tbody>
+		<!-- Test if atleast one ip is banned -->
+		<?php if (empty($ip)) { ?>
+			<table class="table table-striped table-bordered mt-3">
+				<tbody>
+					<!-- None banned IP -->
+					<tr>
+						<td>Aucune adresse IP bannie</td>
+					</tr>
+		<?php
+		}
+		else
+		{
+		?>
+			<!-- Display banned ip -->
 			<?php foreach ($ip as $ips): ?>
-				<!-- Display ip adress -->
-				<tr>
-					<td class="td-hidden table15pourcent">Adresse IP</td>
-					<td data-label="Adresse Ip"><?= esc($ips->adresse_ip) ?></td>
-					<td data-label="Action" class="actionend">
+				<table class="table table-striped table-bordered mt-3">
+					<tbody>
+						<tr>
+							<td class="td-hidden table15pourcent">Adresse IP</td>
+							<td data-label="Adresse Ip"><?= esc($ips->adresse_ip) ?></td>
+							<td data-label="Action" class="actionend">
 
-						<form method="post" action="<?= site_url('Ip/update/'.$ips->id) ?>">
-							<!-- Enabled ip button -->
-							<input type="hidden" name="nb_echec" id="nb_echec" value="0"?>
+								<form method="post" action="<?= site_url('Ip/update/'.$ips->id) ?>">
+									<!-- Enabled ip button -->
+									<input type="hidden" name="nb_echec" id="nb_echec" value="0"?>
 
-							<!-- Redirection button -->
-							<input type="hidden" name="redirect_url" value="<?= current_url(); ?>">
-							<button type="submit" class="btn btn-danger btn-sm"> Rétablir IP </button>
-						</form>
-					</td>
-				</tr>
+									<!-- Redirection button -->
+									<input type="hidden" name="redirect_url" value="<?= current_url(); ?>">
+									<button type="submit" class="btn btn-danger btn-sm"> Rétablir IP </button>
+								</form>
+							</td>
+						</tr>
 			<?php endforeach; ?>
+			<?php
+			}
+			?>
+
 		</tbody>
 	</table>
 
@@ -40,27 +56,43 @@
 
 	<h4> Utilisateur Bannis </h4>
 
-	<!-- Display banned user -->
-	<table class="table table-striped table-bordered mt-3 noBorder">
-		<tbody>
+	<!-- Test if atleast one user is banned -->
+	<?php if (empty($user)) { ?>
+		<table class="table table-striped table-bordered mt-3">
+				<tbody>
+					<!-- None banned user -->
+					<tr>
+						<td>Aucune utilisateur banni</td>
+					</tr>
+		<?php
+		}
+		else
+		{
+		?>
+			<!-- Display banned user -->
 			<?php foreach ($user as $users): ?>
-				<!-- Display users -->
-				<tr>
-					<td data-label="Nom" class="table15pourcent"><?= esc($users->nom) ?></td>
-					<td data-label="Prénom"><?= esc($users->prenom) ?></td>
-					<td data-label="Action" class="actionend">
+				<table class="table table-striped table-bordered mt-3 noBorder">
+					<tbody>
+						<tr>
+							<td data-label="Nom" class="table15pourcent"><?= esc($users->nom) ?></td>
+							<td data-label="Prénom"><?= esc($users->prenom) ?></td>
+							<td data-label="Action" class="actionend">
 
-						<form method="post" action="<?= site_url('User/update/'.$users->id) ?>">
-							<!-- Enabled account button -->
-							<input type="hidden" name="actif" id="actif" value="1">
+								<form method="post" action="<?= site_url('User/update/'.$users->id) ?>">
+									<!-- Enabled account button -->
+									<input type="hidden" name="actif" id="actif" value="1">
 
-							<!-- Redirection button -->
-							<input type="hidden" name="redirect_url" value="<?= current_url(); ?>">
-							<button type="submit" class="btn btn-danger btn-sm"> Rétablir utilisateur </button>
-						</form>
-					</td>
-				</tr>
+									<!-- Redirection button -->
+									<input type="hidden" name="redirect_url" value="<?= current_url(); ?>">
+									<button type="submit" class="btn btn-danger btn-sm"> Rétablir utilisateur </button>
+								</form>
+							</td>
+						</tr>
 			<?php endforeach; ?>
+			<?php
+			}
+			?>
+
 		<t/body>
 	</table>
 
