b3f032e9f1bb78d6bde97c853345eca44d6beaed
DineJ <dinejridi@gmail.com>
2025-08-19 08:39:12 +0200

    Login via webcam (scan a QR code). The password is no longer saved each time you use it

--- app/Views/Login/login.php
+++ app/Views/Login/login.php
@@ -1,23 +1,16 @@
- <!-- Extend base layout -->
 <?= $this->extend('layouts/main') ?>
-
- <!-- Start main content section -->
 <?= $this->section('content') ?>
 
 <h2>Login</h2>
 
-<!-- Login form -->
-<form method="post" action="<?= site_url('Login/log') ?>" class="mb-3">
+<form method="post" id="form-login" name="form-login" action="<?= site_url('Login/log') ?>" autocomplete="off" class="mb-3">
 	<div class="mb-2">
-
 		<!-- Password input with length constraints -->
+		<div id="qr-reader" style="width:300px; display:block;"></div>
 		<input type="password" id="clef_connexion" name="clef_connexion" class="form-control"
 				minlength="16" maxlength="32"
-				placeholder="Mot de passe entre 16 et 32 caracteres" required>
+				placeholder="Mot de passe entre 16 et 32 caracteres" autocomplete="new-password" required>
 	</div>
-
-	<!-- Submit button -->
-	<button type="submit" class="btn btn-primary">Connexion</button>
 
 	<!-- Display flash error message if exists -->
 	<?php if (session()->getFlashdata('error')): ?>
@@ -25,7 +18,42 @@
 			<?= session()->getFlashdata('error') ?>
 		</div>
 	<?php endif; ?>
+
 </form>
 
-<!-- End content section -->
+
+<script src="https://unpkg.com/html5-qrcode"></script>
+<script>
+	document.addEventListener("DOMContentLoaded", () => {
+		const qrReader = document.getElementById("qr-reader");
+		const qrLogin = document.getElementById("form-login");
+		const qrResult = document.getElementById("clef_connexion");
+		const html5QrCode = new Html5Qrcode("qr-reader");
+		const config = { fps: 10, qrbox: 250 };
+
+		function onScanSuccess(decodedText) {
+			console.log("QR Code détecté :", decodedText);
+			qrResult.value = decodedText.replace(/[\r\n]/g, '');
+			qrLogin.requestSubmit();
+
+			html5QrCode.stop()
+				.then(() => console.log("Scanner arrêté"))
+				.catch(err => console.error("Erreur arrêt scanner :", err));
+		}
+
+			Html5Qrcode.getCameras()
+				.then(cameras => {
+					if(cameras.length > 0) {
+						html5QrCode.start(cameras[0].id, config, onScanSuccess)
+							.then(() => console.log("Scanner démarré"))
+							.catch(err => console.error("Erreur démarrage :", err));
+					} else {
+						alert("Aucune caméra détectée !");
+					}
+				})
+				.catch(err => console.error("Erreur détection caméra :", err));
+	});
+</script>
+
+
 <?= $this->endSection() ?>
