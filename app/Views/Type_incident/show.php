d1435f1111c9307cd39aa91ed8eb22580ef8df64
DineJ <dinejridi@gmail.com>
2025-09-08 13:20:02 +0200

    I shouldnt have deleted those files

---
+++ app/Views/Type_incident/show.php
@@ -0,0 +1,41 @@
+<?= $this->extend('layouts/main') ?>
+<?= $this->section('content') ?>
+
+<div class="container mt-5">
+<h2>Détails de Type_incident</h2>
+
+<div class="table-responsive">
+	<table class="table table-striped table-bordered mt-3">
+		<tbody>
+
+			<!-- Display nom -->
+			<tr>
+				<td class="td-hidden">nom</td>
+				<td data-label="nom"><?= $item->nom ?></td>
+			</tr>
+
+			<!-- Display critique -->
+			<tr>
+				<td class="td-hidden">critique</td>
+				<td data-label="critique"><?= $item->critique ? 'Oui' : 'Non' ?></td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+
+
+<div>
+	<form method="post" action="<?= site_url('Type_incident/update/'.$item->id) ?>">
+
+		<!-- Redirection button to edit user form -->
+		<a href="<?= site_url('Type_incident/edit/'.$item->id) ?>" class="btn btn-warning">Modifier</a>
+
+		<!-- Disabled account button -->
+		
+	</form>
+</div>
+</br>
+
+<!-- Redirection button -->
+<a href="<?= site_url('Type_incident') ?>" class="btn btn-secondary">Retour</a>
+<?= $this->endSection() ?>
