bfd06f07eda0486f24083432d56fe285e7d274af
DineJ <dinejridi@gmail.com>
2025-10-30 08:52:49 +0100

    Create a flexible regex for 'Plaque' field. Database will automaticaly handle the format AB-123-CD using a trigger

--- app/Views/Vehicule/create.php
+++ app/Views/Vehicule/create.php
@@ -6,34 +6,28 @@
 <form method="post" action="<?= site_url('Vehicule/store/') ?>">
 
 	<!-- Type plaque -->
-	<label>plaque</label>
-	<input type="text" oninput="setUpper(document.getElementById('plaque'));" id="plaque" name="plaque" value="<?= isset($item) ? $item->plaque : '' ?>" class="form-control" required>
+	<label>Plaque</label>
+	<input type="text" oninput="setUpper(document.getElementById('plaque'));" pattern="^[a-zA-Z]{2}[\s\-]*[0-9]{3}[\s\-]*[a-zA-Z]{2}$" id="plaque" name="plaque" value="<?= isset($item) ? $item->plaque : '' ?>" class="form-control" required>
 
 	<!-- Type marque -->
-	<label>marque</label>
+	<label>Marque</label>
 	<input type="text" oninput="setUpper(document.getElementById('marque'));" id="marque" name="marque" value="<?= isset($item) ? $item->marque : '' ?>" class="form-control" required>
 
 	<!-- Type modele -->
-	<label>modele</label>
+	<label>Modele</label>
 	<input type="text" oninput="setUpper(document.getElementById('modele'));" id="modele" name="modele" value="<?= isset($item) ? $item->modele : '' ?>" class="form-control" required>
 
 	<!-- Type date -->
-	<label>date_achat</label>
+	<label>Date achat</label>
 	<input type="date" id="date_achat" name="date_achat" value="<?= isset($item) ? $item->date_achat : '' ?>" class="form-control" required>
 
 	<!-- Type date -->
-	<label>date_immat</label>
+	<label>Date immat</label>
 	<input type="date" id="date_immat" name="date_immat" value="<?= isset($item) ? $item->date_immat : '' ?>" class="form-control" required>
 
 	<!-- Type date -->
-	<label>ct</label>
+	<label>CT</label>
 	<input type="date" id="ct" name="ct" value="<?= isset($item) ? $item->ct : '' ?>" class="form-control" required>
-
-	<!-- Check your actif -->
-	<label>actif</label>
-	<div>
-		<input type="checkbox" id="actif" name="actif" value="1" <?= (isset($item) && $item->actif) ? 'checked' : '' ?>>
-	</div>
 
 	<!-- Redirection button -->
 	<a href="<?= site_url('Vehicule') ?>" class="btn btn-secondary mt-3">Retour</a>
