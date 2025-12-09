ea38b1e55cb6fbc555cff41ff3a7f2238d574505
DineJ <dinejridi@gmail.com>
2025-12-09 14:27:51 +0000

    Indentation

--- app/Views/Admin/admin.php
+++ app/Views/Admin/admin.php
@@ -18,16 +18,16 @@ function paginateNumber (&$count, $items = 3)
 function bannedDatas ($entity,$entity_name, $columns_entity, $resetData, $valueResetData)
 { ?>
 	<div class="table-responsive">
-	<h4 class="centerTitle"><?= $entity_name ?> bannis</h4>
+		<h4 class="centerTitle"><?= $entity_name ?> bannis</h4>
 		<table class="table table-striped table-bordered mt-3">
 			<tbody>
-		<!-- Test if atleast one <?= $entity_name ?> is banned -->
+				<!-- Test if atleast one <?= $entity_name ?> is banned -->
 				<?php if (empty($entity))
 				{ ?>
 
-				<!-- None banned <?= $entity_name ?> -->
+					<!-- None banned <?= $entity_name ?> -->
 					<tr>
-					<td class="labelAlign"> Aucun <?= $entity_name ?> banni</td>
+						<td class="labelAlign"> Aucun <?= $entity_name ?> banni</td>
 					</tr>
 				<?php
 				}
@@ -43,44 +43,46 @@ function bannedDatas ($entity,$entity_name, $columns_entity, $resetData, $valueR
 		<a href="<?= site_url(''.$entity_name.'') ?>" class="btn btn-secondary">Retour vers <?= $entity_name ?></a>
 	</div>
 	</br></br>
-<?php }
+<?php
+}
 
 # Display datas columns
 function entityColumns($entity, $entity_name, $columns_entity, $resetData, $valueResetData)
 {
-		array_map(function($item) use ($columns_entity, $entity_name, $resetData, $valueResetData)
+	array_map(function($item) use ($columns_entity, $entity_name, $resetData, $valueResetData)
 	{
 		static $count = 0;
 		if (paginateNumber($count))
 		{
 
-		$columns_entity;
-		$html = '<tr>'
-			.'<td class="td-hidden table15pourcent">'.$entity_name.'</td>';
-		$id = 0;
+			$columns_entity;
+			$html = '<tr>'
+				.'<td class="td-hidden table15pourcent">'.$entity_name.'</td>';
+			$id = 0;
 
-		foreach ($item as $c => $v) :
+			foreach ($item as $c => $v) :
 
-			if ($c == 'id'):
-				$id = $v;
-			endif;
+				if ($c == 'id'):
+					$id = $v;
+				endif;
 
-			if (in_array($c, $columns_entity)): 	
-				$html .= '<td data-label="'.$c.'">'.esc($v).'</td>';
-			endif;
+				if (in_array($c, $columns_entity)):
+					$html .= '<td data-label="'.$c.'">'.esc($v).'</td>';
+				endif;
 
 			endforeach;
-		$html .= '<td data-label="Action" class="actionend">'
-		.'<form method="post" action="'. site_url(''.$entity_name.'/update/'.$id) .'">'
-		.'<!-- Enabled account button -->'
-		.'<input type="hidden" name="'.$resetData.'" id="'.$resetData.'" value="'.$valueResetData.'">'
-		.'<!-- Redirection button -->'
-		.'<input type="hidden" name="redirect_url" value="'. current_url() .'">'
-		.'<button type="submit" class="btn btn-danger btn-sm"> Rétablir '.$entity_name.' </button>'
-		.'</form>'
-		.'</td>'
-		.'</tr>';
-		echo $html;
+
+			$html .= '<td data-label="Action" class="actionend">'
+			.'<form method="post" action="'. site_url(''.$entity_name.'/update/'.$id) .'">'
+			.'<!-- Enabled account button -->'
+			.'<input type="hidden" name="'.$resetData.'" id="'.$resetData.'" value="'.$valueResetData.'">'
+			.'<!-- Redirection button -->'
+			.'<input type="hidden" name="redirect_url" value="'. current_url() .'">'
+			.'<button type="submit" class="btn btn-danger btn-sm"> Rétablir '.$entity_name.' </button>'
+			.'</form>'
+			.'</td>'
+			.'</tr>';
+			echo $html;
 		}
 	}, $entity);
 }
