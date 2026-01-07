4be1e99158255e15cd18b95878058300f3737998
DineJ <dinejridi@gmail.com>
2026-01-07 11:39:11 +0000

    Formalized labels

--- app/Views/Lieu/create.php
+++ app/Views/Lieu/create.php
@@ -5,24 +5,24 @@
 
 <form method="post" action="<?= site_url('Lieu/store/') ?>">
 
-	<!-- Type nom_lieu -->
-	<label>nom_lieu</label>
+	<!-- Type city's name -->
+	<label>Ville</label>
 	<input type="text" onchange="setUpper(document.getElementById('nom_lieu'));" id="nom_lieu" name="nom_lieu" value="<?= isset($item) ? $item->nom_lieu : '' ?>" class="form-control" required>
 
-	<!-- Type code_postal -->
-	<label>code_postal</label>
+	<!-- Type postal code -->
+	<label>Code postal</label>
 	<input type="text" onchange="setUpper(document.getElementById('code_postal'));" id="code_postal" name="code_postal" value="<?= isset($item) ? $item->code_postal : '' ?>" class="form-control" required>
 
-	<!-- Type number -->
-	<label>numero</label>
+	<!-- Type street number  -->
+	<label>Numéro</label>
 	<input type="number" id="numero" name="numero" value="<?= isset($item) ? $item->numero : '' ?>" class="form-control" required>
 
-	<!-- Type adresse -->
-	<label>adresse</label>
+	<!-- Type address -->
+	<label>Adresse</label>
 	<input type="text" onchange="setUpper(document.getElementById('adresse'));" id="adresse" name="adresse" value="<?= isset($item) ? $item->adresse : '' ?>" class="form-control" required>
 
-	<!-- Check your actif -->
-	<label>actif</label>
+	<!-- Site is active or not -->
+	<label>Actif</label>
 	<div>
 		<input type="checkbox" id="actif" name="actif" value="1" <?= (isset($item) && $item->actif) ? 'checked' : '' ?>>
 	</div>
