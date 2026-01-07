1c4c8dc47f6c6f0c65698e172bd06286aa368d7f
DineJ <dinejridi@gmail.com>
2026-01-07 13:19:10 +0000

    Formalized labels

--- app/Views/Lieu/edit.php
+++ app/Views/Lieu/edit.php
@@ -5,32 +5,25 @@
 
 <form method="post" action="<?= site_url('Lieu/update/'.$item->id) ?>" onsubmit="return validateForm()">
 
-	<!-- Type nom_lieu -->
-	<label>nom_lieu</label>
+	<!-- Type city's name -->
+	<label>Ville</label>
 	<input type="text" onchange="setUpper(document.getElementById('nom_lieu'));" id="nom_lieu" name="nom_lieu" value="<?= isset($item) ? $item->nom_lieu : '' ?>" class="form-control" required>
 	<input type="hidden" id="oldnom_lieu" name="oldnom_lieu" value="<?= isset($item) ? $item->nom_lieu : '' ?>">
 
-	<!-- Type code_postal -->
-	<label>code_postal</label>
+	<!-- Type postal code -->
+	<label>Code postal</label>
 	<input type="text" onchange="setUpper(document.getElementById('code_postal'));" id="code_postal" name="code_postal" value="<?= isset($item) ? $item->code_postal : '' ?>" class="form-control" required>
 	<input type="hidden" id="oldcode_postal" name="oldcode_postal" value="<?= isset($item) ? $item->code_postal : '' ?>">
 
-	<!-- Type number -->
-	<label>numero</label>
+	<!-- Type street number -->
+	<label>Numéro</label>
 	<input type="number" id="numero" name="numero" value="<?= isset($item) ? $item->numero : '' ?>" class="form-control" required>
 	<input type="hidden" id="oldnumero" name="oldnumero" value="<?= isset($item) ? $item->numero : '' ?>">
 
-	<!-- Type adresse -->
-	<label>adresse</label>
+	<!-- Type address -->
+	<label>Adresse</label>
 	<input type="text" onchange="setUpper(document.getElementById('adresse'));" id="adresse" name="adresse" value="<?= isset($item) ? $item->adresse : '' ?>" class="form-control" required>
 	<input type="hidden" id="oldadresse" name="oldadresse" value="<?= isset($item) ? $item->adresse : '' ?>">
-
-	<!-- Check your actif -->
-	<label>actif</label>
-	<div>
-		<input type="checkbox" id="actif" name="actif" value="1" <?= (isset($item) && $item->actif) ? 'checked' : '' ?>>
-	</div>
-	<input type="hidden" id="oldactif" name="oldactif" value="<?= isset($item) ? $item->actif : '' ?>">
 
 	<!-- Redirection button -->
 	<a href="<?= site_url('Lieu') ?>" class="btn btn-secondary mt-3">Retour</a>
