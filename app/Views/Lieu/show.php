7de43ae3c366577e3bded0024b64f19c89f535cb
DineJ <dinejridi@gmail.com>
2026-01-07 12:00:48 +0000

    Formalized buttons classification

--- app/Views/Lieu/show.php
+++ app/Views/Lieu/show.php
@@ -45,16 +45,18 @@
 <div>
 	<form method="post" action="<?= site_url('Lieu/update/'.$item->id) ?>">
 
+
+		<!-- Redirection button -->
+		<a href="<?= site_url('Lieu') ?>" class="btn btn-secondary">Retour</a>
+
 		<!-- Redirection button to edit user form -->
 		<a href="<?= site_url('Lieu/edit/'.$item->id) ?>" class="btn btn-warning">Modifier</a>
 
 		<!-- Disabled account button -->
 		<input type="hidden" name="actif" id="actif" value="<?= $item->actif ? 0 : 1 ?>">
 		<button type="submit" class="btn <?= $item->actif ? 'btn-danger' : 'btn-success' ?>"> <?= $item->actif ? 'Rendre inactif' : 'Rendre actif' ?></button>
+
 	</form>
 </div>
-</br>
 
-<!-- Redirection button -->
-<a href="<?= site_url('Lieu') ?>" class="btn btn-secondary">Retour</a>
 <?= $this->endSection() ?>
