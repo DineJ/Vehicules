f67fad5f0ef3a41a983c08f291bea97ac27fa97f
DineJ <dinejridi@gmail.com>
2026-01-28 09:21:43 +0000

    Change the layout organization

--- app/Views/Non_admin/home.php
+++ app/Views/Non_admin/home.php
@@ -6,16 +6,16 @@
 
 <h2> Espace des conducteurs</h2>
 
-<div>
-	<a href="<?= $missionCommence  ? 'Mission/fin' : 'Mission/debut' ?>" class="btn btn-warning"> <?= $missionCommence ? 'Finir la mission' : 'Commencer la mission' ?></a>
-	<a href="'Incident/declarer" class="btn btn-danger">Déclarer un incident</a>
-</div>
-
 <?php if ($missionCommence)
 {
-	createSection($missions, 'Mission', 'Non_admin', ['nom_complet' => 'Conducteur', 'plaque' => 'Plaque', 'lieu_depart' => 'Lieu de départ', 'lieu_arrive' => 'Lieu arrivé', 'motif' => 'Motif', 'date_depart' => 'Date départ', 'km_depart' => 'Km départ'], '0', '1');
+	createSection($missions, 'Mission', 'Non_admin', ['nom_complet' => 'Conducteur', 'plaque' => 'Plaque', 'lieu_depart' => 'Lieu de départ', 'lieu_arrive' => 'Lieu arrivé', 'motif' => 'Motif', 'date_depart' => 'Date départ', 'km_depart' => 'Km départ'], '0', '0');
 }
 ?>
+<br>
+<div>
+	<a href="<?= $missionCommence  ? 'Mission/end/'.session()->get('user')['id']) : 'Mission/debut' ?>" class="btn btn-warning"> <?= $missionCommence ? 'Finir la mission' : 'Commencer la mission' ?></a>
+	<a href="'Incident/declarer" class="btn btn-danger">Déclarer un incident</a>
+</div>
 
 <script src="<?= base_url('js/popupModal.js') ?>"></script>
 <?= $this->endSection() ?>
