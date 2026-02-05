d18d5c4d0ea094f3ec071a26e0cfbd17f5551aa4
DineJ <dinejridi@gmail.com>
2026-02-05 06:59:19 +0000

    Added setUpper() function

--- app/Views/Permis/create.php
+++ app/Views/Permis/create.php
@@ -9,7 +9,7 @@
         <input type="hidden"id="id_user" name="id_user" value="<?= esc($userId ?? '') ?>">
 
 	<label>Numéro</label>
-	<input type='text' pattern="^[0-9A-Z]{9}$" id='num_permis' name='num_permis' value='<?= isset($item) ? $item->num_permis : '' ?>' class='form-control' required>
+	<input type='text' pattern="^[0-9A-Z]{9}$" oninput="setUpper(document.getElementById('num_permis'));" id='num_permis' name='num_permis' value='<?= isset($item) ? $item->num_permis : '' ?>' class='form-control' required>
 
 	<label>Date d'obtention</label>
 	<input type='date' id='date_permis' name='date_permis' value='<?= isset($item) ? $item->date_permis : '' ?>' class='form-control' required>
@@ -32,5 +32,7 @@
 	<a href="<?= site_url('User/show/'.$userId) ?>" id="btnRetour" class="btn btn-secondary mt-3">Retour</a>
 	<button type="submit" class="btn btn-primary mt-3">Enregistrer</button>
 </form>
+
+<script src="<?= base_url('js/main.js') ?>"></script>
 
 <?= $this->endSection() ?>
