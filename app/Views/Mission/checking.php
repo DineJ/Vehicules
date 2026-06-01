11b0d894f7c942589743264b76b016514ba82235
DineJ <dinejridi@gmail.com>
2026-06-01 08:37:35 +0200

    Update UX UI by updating pictures and labels

--- app/Views/Mission/checking.php
+++ app/Views/Mission/checking.php
@@ -1,8 +1,8 @@
 <?= $this->extend('layouts/main') ?>
 <?= $this->section('content') ?>
 
-<?php vehicle_checking_form("un","block","none","deux","Usure des pneus (visuel)",["front_tires" => "Pneux avant","rear_tires" => "Pneux arrière"], ["Usés","Pas usés"], ["oeil.png","pneu_usé.png"]) ?>
-<?php vehicle_checking_form("deux","none","un","trois","Pression des pneus (visuel)",["front_tires" => "Pneux avant","rear_tires" => "Pneux arrière"], ["Usés","Pas usés"], ["oeil.png","pneu_usé.png"]) ?>
+<?php vehicle_checking_form("un","block","none","deux","Usure des pneus (visuel)",["front_tires" => "Pneux avant","rear_tires" => "Pneux arrière"], ["Usés","Pas usés"], ["oeil.png","pneu_use.png"]) ?>
+<?php vehicle_checking_form("deux","none","un","trois","Pression des pneus (visuel)",["front_tires" => "Pneux avant","rear_tires" => "Pneux arrière"], ["Dégonflés","Gonflés"], ["oeil.png","pneu_degonfle.png"]) ?>
 <?php vehicle_checking_form("trois","none","deux","none","Contrôle des niveaux",["engine_oil" => "Huile moteur","coolant_fluid" => "Liquide refroidissement", "brake_fluid" => "Liquide de frein", "washer_fluid" => "Liquide lave-glace"], ["À remplir","Ok"], ["oeil.png","jauge.png"]) ?>
 
 <script>
